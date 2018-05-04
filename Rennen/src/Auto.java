a898e809406ae0cabac42d59929faa125ef5a0d2
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A89.2631.skool.net>
2018-05-04 09:08:54 +0200

    String mit Autodaten als return

--- Rennen/src/Auto.java
+++ Rennen/src/Auto.java
@@ -2,6 +2,7 @@ import java.util.Random;
 
 public class Auto {
 	
+	int nummer; //Identifizierung unterschiedliche Autos
 	double vmax; //Variablen werden bestimmt, nicht initialisiert
 	double speed;//eigentliche Geschwindigkeit, bei Fehlern geändert, sonst vmax
 	double capacity;
@@ -9,7 +10,8 @@ public class Auto {
 	double energy_remaining;
 	
 
-	public Auto(double vmax, double capacity, double consumption){ //Konstruktor Objekt Auto, Eigenschaften werden übergeben
+	public Auto(int nummer, double vmax, double capacity, double consumption){ //Konstruktor Objekt Auto, Eigenschaften werden übergeben
+		this.nummer = nummer; //individuelle Nummern
 		this.vmax = vmax; //eigene Geschwindigkeit (sonst alle gleich)
 		this.capacity = capacity;
 		this.consumption = consumption;
@@ -21,14 +23,28 @@ public class Auto {
 	
 	}
 	
-	public void failure(){
+	public void failure(){ //Fehler (Auto fährt langsamer)
 		
-		Random random = new Random(10);
+		Random random = new Random(10); //zufällliger Wert, bestimmt ob und wie gravierend Fehler ist
 		int failure = random.nextInt(10);
-		speed = vmax-failure;
+		speed = vmax-failure; //aktuelle Geschwindigkeit ist Maximalgeschwindigkeit-Fehlerfaktor (0-10)
 	}
 
-	public void aufladen(){
-	
+	public void aufladen(){ //Aufladen des Akkus
+		energy_remaining=capacity; //Akku wird wieder voll (kapazitätat)
+		speed=speed-80; //Durchschnittsgeschwindigkeit für Runde sinkt, da Aufladen zeit braucht
 	}
+	
+	public String toString(){ //Daten für Auto werden ausgegeben
+		return "Startnummer :" +nummer+ //\n ist Zeilenumbruch; großer String als Rückgabewert
+				"\n Vmax:"+vmax+
+				"\n Derzeitige Durchschnittsgeschwindigkeit: "+speed+
+				"\n Akkukapazität: " +capacity+
+				"\n Restenergie: " +energy_remaining+
+				"\n Verbrauch auf 100 km:" +consumption;
+		
+		
+		
+	}
+	
 }
