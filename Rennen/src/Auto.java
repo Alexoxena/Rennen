05d2fc1fda9ccbbd8d39fef2ca7906aff0626461
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A89.2631.skool.net>
2018-05-04 08:46:46 +0200

    3rd commmit, weiter erstellung Auto, Fehlereinbau

--- Rennen/src/Auto.java
+++ Rennen/src/Auto.java
@@ -1,6 +1,34 @@
+import java.util.Random;
 
 public class Auto {
 	
-	double vmax;
+	double vmax; //Variablen werden bestimmt, nicht initialisiert
+	double speed;//eigentliche Geschwindigkeit, bei Fehlern geändert, sonst vmax
+	double capacity;
+	double consumption;
+	double energy_remaining;
 	
+
+	public Auto(double vmax, double capacity, double consumption){ //Konstruktor Objekt Auto, Eigenschaften werden übergeben
+		this.vmax = vmax; //eigene Geschwindigkeit (sonst alle gleich)
+		this.capacity = capacity;
+		this.consumption = consumption;
+		this.energy_remaining = this.capacity;//werden voll aufgeladen: Akku voll(eigene Kapazität, da nicht allgemein)
+	}
+
+
+	public void fahren(){
+	
+	}
+	
+	public void failure(){
+		
+		Random random = new Random(10);
+		int failure = random.nextInt(10);
+		speed = vmax-failure;
+	}
+
+	public void aufladen(){
+	
+	}
 }
